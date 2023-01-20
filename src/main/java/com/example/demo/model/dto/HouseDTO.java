6f0a6c3827b007d559b60f7417c319d9d85c7da8
Kharatian <kharatian_arthur@mail.ru>
2023-01-20 10:18:24 +0300

    set privets

--- src/main/java/com/example/demo/model/dto/HouseDTO.java
+++ src/main/java/com/example/demo/model/dto/HouseDTO.java
@@ -1,11 +1,14 @@
 package com.example.demo.model.dto;
 
 import com.example.demo.model.enums.MaterialType;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class HouseDTO {
 
     MaterialType material;
--- src/main/java/com/example/demo/model/dto/UserDTO.java
+++ src/main/java/com/example/demo/model/dto/UserDTO.java
@@ -1,13 +1,16 @@
 package com.example.demo.model.dto;
 
 import com.example.demo.model.enums.Gender;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 
 @Getter
 @Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDTO {
 
     Integer age;
