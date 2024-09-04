d3e660ee777df9a4b03023b7cf9faaa5544ab7de
Kharatian <kharatian_arthur@mail.ru>
2024-09-04 21:07:10 +0300

    lesson 2

--- src/main/java/com/example/demo/constants/Constants.java
+++ src/main/java/com/example/demo/constants/Constants.java
@@ -2,5 +2,5 @@ package com.example.demo.constants;
 
 public interface Constants {
     String API = "/api";
-    String USER_API = API + "/users";
+    String USERS = API + "/users";
 }
--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -2,40 +2,60 @@ package com.example.demo.controllers;
 
 import com.example.demo.model.dto.request.UserInfoRequest;
 import com.example.demo.model.dto.response.UserInfoResponse;
+import com.example.demo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
+import javax.validation.Valid;
 import java.util.List;
 
-import static com.example.demo.constants.Constants.USER_API;
+import static com.example.demo.constants.Constants.USERS;
 
+@Tag(name = "Пользователи")
 @RestController
-@RequestMapping(USER_API)
+@RequestMapping(USERS)
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
     @PostMapping
-    public UserInfoResponse createUser(@RequestBody UserInfoRequest request) {
-        return new UserInfoResponse();
+    @Operation(summary = "Создать пользователя")
+    public UserInfoResponse createUser(@RequestBody @Valid UserInfoRequest request) {
+        return userService.createUser(request);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получить пользователя по ID")
     public UserInfoResponse getUser(@PathVariable Long id) {
-        return new UserInfoResponse();
+        return userService.getUser(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Обновить пользователя по ID")
     public UserInfoResponse updateUser(@PathVariable Long id, @RequestBody UserInfoRequest request) {
-        return new UserInfoResponse();
+        return userService.updateUser(id, request);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удалить пользователя по ID")
     public void deleteUser(@PathVariable Long id) {
-
+        userService.deleteUser(id);
     }
 
     @GetMapping("/all")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успех"),
+            @ApiResponse(responseCode = "404", description = "Не найден"),
+            @ApiResponse(responseCode = "401", description = "Не авторизован"),
+    })
+    @Operation(summary = "Получить список пользователей")
     public List<UserInfoResponse> getAllUsers() {
-        return Collections.singletonList(new UserInfoResponse());
+        return userService.getAllUsers();
     }
 
 }
