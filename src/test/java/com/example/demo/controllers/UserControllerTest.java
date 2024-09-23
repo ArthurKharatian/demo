9ca8c8079d3ed3cc3717512c12542127c916b604
Kharatian <kharatian_arthur@mail.ru>
2024-09-23 21:31:53 +0300

    lesson 6

---
+++ src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -0,0 +1,78 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.dto.request.UserInfoRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.ConfigurableMockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static com.example.demo.constants.Constants.USERS;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserControllerTest {
+
+    UserInfoRequest user = new UserInfoRequest();
+
+    {
+        user.setEmail("test@test.com");
+        user.setFirstName("Ivan");
+        user.setLastName("Ivanov");
+    }
+
+
+    MockMvc mockMvc;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Rule
+    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation("target/generated-snippets");
+
+
+    @Before
+    public void setUp() {
+        ConfigurableMockMvcBuilder builder = MockMvcBuilders.webAppContextSetup(this.webApplicationContext)
+                .apply(documentationConfiguration(this.restDocumentation));
+
+        this.mockMvc = builder.build();
+    }
+
+
+    @Test
+    @SneakyThrows
+    public void createUser() {
+        String content = objectMapper.writeValueAsString(user);
+        System.out.println(content);
+
+        mockMvc.perform(post(USERS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.firstName").value(user.getFirstName()))
+                .andExpect(jsonPath("$.lastName").value(user.getLastName()))
+                .andDo(document(USERS.replace("/", "")));
+    }
+
+}
